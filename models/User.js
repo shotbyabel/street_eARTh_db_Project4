31f1553f34c6b1444989a427ed40d77f9fafe89a
kimo5532 <shotbyabel@gmail.com>
2015-09-05 14:28:42 -0700

    fix bcrypt npm issue and set up User model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema,
+    bcrypt   = require('bcryptjs');
+
+//user schema
+var UserSchema  = new Schema({
+    name: String,
+    username: { type: String, require: true, index: {unique: true }}, 
+    password: { type: String, require: true, select: false }
+
+});
+
+// hash the psw before user is saved
+UserSchema.pre('save', function (next) {
+  var user = this;
+//hash psw only if the psw has been changed or user is new
+  if (!user.isModified('password')) return next();
+//generate the hash
+bcrypt.hash(user.password, null, null, function (err, hash) {
+  if (err) return next (err);
+  //changed the password to the hashed version
+  user.password = hash;
+  next();
+  });
+
+});
+
+//method to compare a given password with the databse hash
+UserSchema.methods.comparePassword = function (password) {
+  var user = this;
+
+  return bcrypt.compareSync (password, user.password);
+
+};
+
+
+//return the model
+module.exports = mongoose.model('User', UserSchema);
+
+
+
+    
