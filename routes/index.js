b76af7d86bb9027989c451e268a83964058c19af
kimo5532 <shotbyabel@gmail.com>
2015-09-08 18:11:03 -0700

    clear up tarnished user api code

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,41 @@
 // import Express and the Express Router
-var express       = require('express'), 
-    router        = express.Router(),
-    bodyParser    = require('body-parser'),
-    User          = require('../models/User'),//require user MODEL
-    port          = process.env.PORT || 8080,
-    passport      = require('passport');
+  var express       = require('express'), 
+      router        = express.Router(),
+      bodyParser    = require('body-parser'),
+      User          = require('../models/User'),//require user MODEL
+      passport      = require('passport');
 
 // import controllers for resources
- var SessionsController = require('../controllers/sessions');
+  var SessionsController = require('../controllers/sessions');
 
 // define routes for our application, and send them to route handlers
-// router.get('/', welcomeController.index);
-   router.get('/', function (req, res) {
-    res.render('index', {user: req.user});
+  router.get('/', function (req, res) {
+  res.render('index', {user: req.user});
 
     });
-
 //basic route for home page
-var app = express();
-
-
-
+  var app = express();
 ///////////////////////////
 ////TWITTER AUTH ROUTE////
-app.get('/',
+/////////////////////////
+  app.get('/',
   function(req, res) {
     res.render('home', { user: req.user });
   });
 
-app.get('/login',
+  app.get('/login',
   function(req, res){
     res.render('login');
   });
 
-app.get('/login/twitter',
+  app.get('/login/twitter',
   passport.authenticate('twitter'));
 
-app.get('/login/twitter/return', 
+  app.get('/login/twitter/return', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
-
 // app.get('/profile',
 //   require('connect-ensure-login').ensureLoggedIn(),
 //   function(req, res){
@@ -51,195 +45,41 @@ app.get('/login/twitter/return',
 /////FACEBOOK Oauth ROUTES////
 //////////////////////////////
 // Facebook routes
-router.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'user_birthday', 'user_location']}));
+  router.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: ['email', 'user_birthday', 'user_location']}));
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook', {successRedirect: '/home', failureRedirect: '/login'}));
+  router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/home', failureRedirect: '/login'}));
 
-function isLoggedIn(request, response, next){
-    if(request.isAuthenticated()){return next();}
-    response.redirect('/login');
-}
 ////////////////////////
 ///GOOGLE AUTH ROUTE////
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
-  function(req, res){
+////////////////////////
+  app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
+    function(req, res){
     // The request will be redirected to Google for authentication, so this
     // function will not be called.
-  });
+  
+    });
 
-app.get('/auth/google/callback', 
+  app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
-  });
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-
-//get an instance of the express router
-var apiRouter = express.Router();
-
-// // route for authenticating users
-// // route to authenticate a user (POST http://localhost:8080/api/authenticate)
-//   apiRouter.post('/authenticate', function (req, res) {
-//   //find the user
-//   //selec the name user name and password only
-//   User.findOne({
-//   username: req.body.username
-//  }).select('name username password').exec(function (err, user) {
-
-//     if (err) throw err;
-
-//     // nop user w that username was found
-//     if (!user) {
-//       res.json({
-//         success: false,
-//         message: 'Authentication failed. User not found.' 
-//       });
-//     } else if (user) {
-
-//    //check if password matches
-//    var validPassword = user.comparePassword(req, body.password);
-//     if (!validPassword) {
-//       res.json({
-//         success: false,
-//         message: 'Authenticate failed. Wrong password.'
-//       });
-//     } else {
-
-//       // if user is found and password is right
-//       //create a token
-//     var token = jwt.sign({
-//       name: user.name,
-//       username: user.username
-//      }, superSecret, {
-//       expiresInMinutes: 1440 // expires in 24 hour
-//      });
-//      //return the info including token as JSON
-//      res.json({
-//       success: true,
-//       message: 'Enjoy your token!',
-//       token: token
-        
-
-//         });
-//       }   
-
-//     }
-
-//   });
-// });
-
-//middleware to use for all request
-apiRouter.use (function (req, res, next) {
-  //do loggin
-  console.log('visitor came to the street earth db!');
-  //authenticate users later
-  next(); //
-
-});
-
-//test route to make sure everying is working
-///accessed at GET http://localhost: /api
-apiRouter.get('/', function (req, res) {
-    res.json({message: 'dope! welcome to my api'});
 
   });
-///REGISTER OUR ROUTES----------------------
-//all of our routes will be prefixed with /api
-app.use('/api', apiRouter);
+
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+
+  });
+
+
 // START THE SERVER
 //==================
-app.listen(port);
-console.log('Magic seem to be happening on the api port ' + port);
-// ///more API ROUTES COMING SOON!////
-// apiRouter.route('/users')//NOT WORKING!!!//
-//   //// create a user (accessed at POST http://localhost:8080/api/users)
-//   .post (function (req, res) {
-//     var user = new User();//create new instance of User model
-//     //set the users info (comes fr. req.)
-//     user.name     = req.body.name;
-//     user.username = req.body.username;
-//     user.password = req.body.password;
-//     ///user SAVE and chek for err
-//     user.save (function(err) {
-//       if (err) {
-//         //duplicate entry
-//         if (err.code == 1100)
-//           return res.json({ success: false, message: 'A user with that username already exists. '});
-//         else 
-//           return res.send(err);  
-
-//       }
-
-//             res.json({ message: 'User created!'});
-
-
-//         });
-//     })
-
-   //get all the users (accessed at GET http://localhost:8080/api/users)
-   // .get(function(req, res) {
-
-   //   User.find(function (err, users) {
-   //     if (err) res.send(err);
-
-   //     // return the users
-   //     res.json(users);
-   //   });
-   // });
-// on routes that end in /users/:user_id
-
-  apiRouter.route('/users/:user_id') ///NOT WORKING
-  // get the user with that id
-// (accessed at GET http://localhost:8080/api/users/:user_id) 
-  .get(function (req, res) {
-    User.findById(req.params.user_id, function (err, user) { 
-      if (err) res.send(err);
-    
-    }); 
-
-  })
-
-// update the user with this id
-  // .put(function (req, res) {
-  //   User.findById(req.params.user_id, function (err, user) {
-
-  //     if (err) return res.send(err);
-
-  //     // set the new user information if it exists in the request
-  //     if (req.body.name) user.name = req.body.name;
-  //     if (req.body.username) user.username = req.body.username;
-  //     if (req.body.password) user.password = req.body.password;
-
-  //     // save the user
-  //     user.save(function(err) {
-  //       if (err) return res.send(err);
-
-  //       // return a message
-  //       res.json({ message: 'User updated!' });
-  //     });
-
-  //    });
-  
-  // })
-
-    // delete the user with this id
-      .delete(function (req, res) {
-        User.remove({
-        _id: req.params.user_id
-      }, function (err, user) {
-        if (err) return res.send(err);
-
-        res.json({ message: 'Successfully deleted' });
-    
-      });
-  
-    });
+// app.listen(port);
+// console.log('' + port);
 
 //*Render sessions controller
 router.get('/login', SessionsController.sessionsNew);
--- server.js
+++ server.js
@@ -10,17 +10,7 @@ var path            = require('path');
 var favicon         = require('serve-favicon');
 var cookieParser    = require('cookie-parser');
 var jwt             = require('jsonwebtoken');
-//OAUTH
-// var FacebookStrategy = require('passport-facebook').Strategy 
-// var TwitterStrategy  = require('passport-twitter').Strategy;
-// var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
-// API Access link for creating client ID and secret:
-// https://code.google.com/apis/console/
-///////////////////
-
-//////////////////
 ///loading routes defined in the /routes folder
-
 var routes          = require('./routes/index');
 var passport        = require('passport');
 var LocalStrategy   = require('passport-local').Strategy;
@@ -31,8 +21,6 @@ var LocalStrategy   = require('passport-local').Strategy;
 var Facebook = require('./config/facebook');
 var Twitter  = require('./config/twitter');
 var Google   = require('./config/google');
-//super secret token
-// var superSecret = 'project4';
 
 // check that MongoD is running...
 require('net').connect(27017, 'localhost').on('error', function() {
@@ -63,65 +51,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   res.setHeader('Access-Control-Allow-Origin', '*');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 //   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-  
-//   next();
-
-// });
-
-// insert middleware that points to our route definitions
-/////////////////////////////////
-////Twitter Config Strategy//////
-// passport.use(new TwitterStrategy({
-//     consumerKey: process.env.TWITTER_KEY,
-//     consumerSecret: process.env.TWITTER_SECRET,
-//     callbackURL: "http://127.0.0.1:3000/oauth/twitter"
-//   },
-
-//   function (token, tokenSecret, profile, cb) {
-
-//     return cb(null, profile);
-  
-//   }));
-
-/////////////////////////////////
-////GOOGLE Config Strategy/////
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: 'http://127.0.0.1:3000/oauth/google'
-// },
-
-// function(accessToken, refreshToken, profile, done) {
-//     // asynchronous verification, for effect...
-//     process.nextTick(function () {
-      
-//       return done(null, profile);
-    
-//     });
-  
-//   }
-
-// ));
-/////////////////////////////////
-////FACEBOOK Config Strategy/////
-// passport.use(new FacebookStrategy({
-//     clientID: process.env.FACEBOOK_APP_ID,
-//     clientSecret: process.env.FACEBOOK_APP_SECRET,
-//     callbackURL: "http://localhost:3000/auth/facebook/callback"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     // asynchronous verification, for effect...
-//     process.nextTick(function () {
-      
-//       // To keep the example simple, the user's Facebook profile is returned to
-//       // represent the logged-in user.  In a typical application, you would want
-//       // to associate the Facebook account with a user record in your database,
-//       // and return that user instead.
-//       return done(null, profile);
-//     });
-//   }
-// ));
-
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
